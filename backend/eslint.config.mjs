7bb913b3a57487816b6c20edcf6478fff53920c9
I-Kail-I <mikailgames121@gmail.com>
2026-05-19 23:51:03 +0800

    style: changing the config, instead of workspace it become to it's own directory

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,10 +2,14 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 
 export default [
+  pluginJs.configs.recommended,  
   {
     files: ["**/*.js"],
-    languageOptions: { globals: { ...globals.node, ...globals.es2021 } },
-    ...pluginJs.configs.recommended,
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2021 },
+      sourceType: "module",
+      ecmaVersion: 2021,
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
