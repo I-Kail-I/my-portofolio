c5d20a5b20de123b62d85bd21c26fe49499acbe3
I-Kail-I <mikailgames121@gmail.com>
2026-06-13 21:56:38 +0800

    feat(backend): add blog and upload routes

--- backend/src/router/route.js
+++ backend/src/router/route.js
@@ -10,6 +10,14 @@ import {
   updateExperience,
   deleteExperience,
 } from '../controller/experience.controller.js';
+import {
+  getBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+} from '../controller/blog.controller.js';
+import { upload, uploadImage } from '../controller/upload.controller.js';
 
 const route = Router();
 
@@ -17,6 +25,8 @@ const route = Router();
 route.post('/login', loginController);
 route.get('/experiences', getExperiences);
 route.get('/experiences/:id', getExperienceById);
+route.get('/blogs', getBlogs);
+route.get('/blogs/:id', getBlogById);
 
 // Protected routes
 route.get('/profile', verifyToken, profileController);
@@ -24,5 +34,11 @@ route.post('/logout', verifyToken, logoutController);
 route.post('/experiences', verifyToken, createExperience);
 route.put('/experiences/:id', verifyToken, updateExperience);
 route.delete('/experiences/:id', verifyToken, deleteExperience);
+route.post('/blogs', verifyToken, createBlog);
+route.put('/blogs/:id', verifyToken, updateBlog);
+route.delete('/blogs/:id', verifyToken, deleteBlog);
+
+// Upload
+route.post('/upload', verifyToken, upload.single('image'), uploadImage);
 
 export default route;
